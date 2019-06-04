2dcc4234946b887eeaf939a391d096e1e9a7efdd
Michael Peters <michaelpeterswa@icloud.com>
2019-06-04 13:14:39 -0700

    added /mt cs chat spammer

--- src/main/java/st/northw/MainCommand.java
+++ src/main/java/st/northw/MainCommand.java
@@ -8,17 +8,51 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainCommand implements CommandExecutor {
 
-    private void intro(){
+    private boolean chatspam = false;
+    final Timer t = new Timer();
+
+    private void intro() {
         String mt_prefix = String.format("%s[%smt%s] ", ChatColor.DARK_BLUE, ChatColor.BLUE, ChatColor.DARK_BLUE);
         Bukkit.broadcastMessage(mt_prefix + ChatColor.WHITE + "MemeTools for Minecraft Fools");
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args) {
+        final String mt_prefix = String.format("%s[%smt%s] ", ChatColor.DARK_BLUE, ChatColor.BLUE, ChatColor.DARK_BLUE);
 
-        intro();
+        if (args.length == 0) {
+            intro();
+        } else if (args.length == 1) {
+            if (args[0].equals("cs")) {
+                chatspam = !chatspam;
+                if (!chatspam){
+                   //sender.sendMessage(mt_prefix + ChatColor.WHITE + "chatspam toggled off");
+                   t.cancel();
+                   t.purge();
+                   //t.cancel(); //IAE, timer already cancelled
+                   //chatspam = !chatspam;
+                }
+                if (chatspam) {
+                   sender.sendMessage(mt_prefix + ChatColor.RED + "chatspam toggled on");
+                   Timer t = new Timer();
+                   t.schedule(new TimerTask() {
+                       @Override
+                       public void run() {
+                           Bukkit.broadcastMessage(mt_prefix + ChatColor.WHITE + "lol get memed on fool");
+                           if(!chatspam){
+                               this.cancel();
+                               sender.sendMessage(mt_prefix + ChatColor.RED + "chatspam successfully turned off");
+                           }
+                       }
+                   }, 0, 2000);
+
+                }
+            }
+        }
         return true;
     }
-
 }
